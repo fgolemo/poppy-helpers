66c233650faa404ad12cb27ed6972a7e8ed35d92
Florian Golemo <fgolemo@gmail.com>
2018-04-20 11:01:31 -0300

    added scaled random reset

--- poppy_helpers/randomizer.py
+++ poppy_helpers/randomizer.py
@@ -7,10 +7,10 @@ from poppy_helpers.constants import SWORDFIGHT_RANDOM_NOISE, REST_POS, MOVES
 
 class Randomizer(object):
 
-    def random_from_rest(self, rest_pos, noise):
+    def random_from_rest(self, rest_pos, noise, scaling = 1.0):
         out = []
         for i in range(6):
-            new_pos = int(rest_pos[i] + np.random.randint(
+            new_pos = int(rest_pos[i] + scaling * np.random.randint(
                 low=noise[i][0],
                 high=noise[i][1],
                 size=1)[0])
